f3c01e4a03fcdbe99a3866d952fd3c60ead70caa
AmerigoBovo97 <amerigobovo@gamil.com>
2024-10-18 17:14:41 +0200

    feat: add html variables on js file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-    
+    //html variables
+    const palindromResult = document.getElementById("palindrom-result"); // html element for the palindrome result output
+    const choosenWord = document.getElementById("word-to-check"); // html element for the word input
+    const wordSubmit = document.getElementById("palindrom-button"); // html element for the word input submit
+    const evenOddResult = document.getElementById("even-odd-result"); // html element for the even odd result output
+    const choosenNumber = document.getElementById("choosen-number"); // html element for the number input
+    const evenOddInput = document.getElementById("even-radio"); // html element for the even input
+    const numberSubmit = document.getElementById("word-to-check"); // html element for the number input submit
+
