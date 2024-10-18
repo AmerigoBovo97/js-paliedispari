5c1bf16a6c2a9e46aebf0077b5766eaf871ca7ca
AmerigoBovo97 <amerigobovo@gamil.com>
2024-10-18 18:59:18 +0200

    feat: add even odd game logic

--- js/main.js
+++ js/main.js
@@ -8,28 +8,53 @@ const evenOddInput = document.getElementById("even-radio"); // html element for 
 const numberForm = document.getElementById("even-odd-form"); // html element for the number input submit
 
 
-// palindome logic =============================================================================================================================
+// palindome logic ============================================================================================================================
 palindromeForm.addEventListener("submit", function(event){
 
     event.preventDefault();
     
-    resultDisplayer(palindromeResult, isPalindrome(choosenWord.value.replace(/\s+/g, "")))
+    resultDisplayer(palindromeResult, isPalindrome(choosenWord.value.replace(/\s+/g, "")));
 
 })
 
 
 function reverseString(stringToReverse){
-    return stringToReverse.split("").reverse().join("")
+    return stringToReverse.split("").reverse().join("");
 }
 
 function isPalindrome(word){
     
-    return word === reverseString(word)
+    return word === reverseString(word);
+}
+
+
+// even odd game logic ========================================================================================================================
+numberForm.addEventListener("submit", function(event){
+
+    event.preventDefault();
+    
+    const num = parseInt(choosenNumber.value);
+
+    resultDisplayer(evenOddResult, evenOdd(num, evenOddInput.checked));
+   
+})
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function isEven(num){
+    return num % 2 === 0;
+}
+
+function evenOdd(num1, even){
+    const sum = isEven(num1 + getRndInteger(1, 5));
+    return (sum && even) || (!sum && !even);
 }
 
 
 //result displayer ============================================================================================================================
-function  resultDisplayer (element, result){
+function  resultDisplayer(element, result){
     
     if (result){
         element.classList.replace(element.classList[0], "bg-success");
